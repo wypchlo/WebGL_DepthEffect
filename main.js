717c6720cd552479e16461f2bbcbd91f0f818176
wypchlo <deron.pl.yt@gmail.com>
2023-09-30 18:41:14 +0200

    Hello this is a very professional comment

--- main.js
+++ main.js
@@ -1,92 +1,24 @@
 "use strict";
 
-function setRectangle(gl, x, y, width, height) {
-    const x1 = x;
-    const y1 = y;
-    const x2 = x + width;
-    const y2 = y + height;
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        x1, y1,
-        x2, y1,
-        x1, y2,
-        x1, y2,
-        x2, y1,
-        x2, y2,
-    ]), gl.STATIC_DRAW);
+function loadImage(src, callback) {
+    var image = new Image();
+    image.src = src;
+    image.onload = callback;
+    return image;
+  }
+
+function loadImages(urls, callback) {
+let images = [];
+const imagesToLoad = urls.length;
+
+const onImageLoad = () => {
+    --imagesToLoad;
+    if (imagesToLoad == 0)
+    callback(images);
+};
+
+for (let i = 0; i < imagesToLoad; ++i) {
+    const image = loadImage(urls[ii], onImageLoad);
+    images.push(image);
 }
-
-function render(gl, image)
-{
-    const program = webglUtils.createProgramFromScripts(gl, ["vertexShader", "fragmentShader"]);
-
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    setRectangle(gl, 0, 0, image.width, image.height);
-
-    const texcoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        0.0,  0.0,
-        1.0,  0.0,
-        0.0,  1.0,
-        0.0,  1.0,
-        1.0,  0.0,
-        1.0,  1.0,
-    ]), gl.STATIC_DRAW);
-
-    const texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-
-    const resolutionLocation = gl.getUniformLocation(program, "u_resolution");
-
-    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-    gl.useProgram(program);
-
-    const sets = [
-        2, //size
-        gl.FLOAT, //type
-        false, //normalize
-        0, //stride
-        0 //offset 
-    ]
-
-    const positionLocation = gl.getAttribLocation(program, "a_position");
-    const texcoordLocation = gl.getAttribLocation(program, "a_texCoord");
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.enableVertexAttribArray(positionLocation);
-    gl.vertexAttribPointer(positionLocation, sets[0], sets[1], sets[2], sets[3], sets[4]);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-    gl.enableVertexAttribArray(texcoordLocation);
-    gl.vertexAttribPointer(texcoordLocation, sets[0], sets[1], sets[2], sets[3], sets[4]);
-
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
-
-function main() 
-{
-    const canvas = document.querySelector("canvas");
-    const gl = canvas.getContext("webgl");
-    if (!gl) console.error("Browser doesn't support WebGL");
-
-    const image = new Image();
-    image.src = "Images/cc_landscape - 2.png";
-    image.onload = () => { render(gl, image); } 
-}
-
-main();
