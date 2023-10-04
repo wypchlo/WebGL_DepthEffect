489125a10826e9196d5fea7f44ce74289380b0a1
wypchlo <deron.pl.yt@gmail.com>
2023-10-04 20:45:07 +0200

    blep

--- main.js
+++ main.js
@@ -3,9 +3,37 @@
 const posVisualizer = document.querySelector("#positionVisualizer");
 const resolution = [window.innerHeight / 9 * 16, window.innerHeight];
 
+function lerp (start, end, amt){
+    return (1 - amt) * start + amt * end
+  }
+
+function normalizeVector2(a, b){
+    let greater
+    if (Math.abs(a) > Math.abs(b))
+        greater = Math.abs(a);
+    else if (Math.abs(a) < Math.abs(b))
+        greater = Math.abs(b);
+    else
+        return [a, b]
+    return [a / greater, b / greater];
+}
+
+let visPos = [0, 0];
+let prevMouse = [0, 0];
+let testMouse = [0, 0];
+
 function setSize() {
     canvas.width = resolution[0];
     canvas.height = resolution[1];
+}
+
+function mouseRender(){
+    let vector = normalizeVector2(testMouse[0] - visPos[0], testMouse[1] - visPos[1]);
+    visPos[0] += vector[0];
+    visPos[1] += vector[1];
+    prevMouse = testMouse;
+    posVisualizer.style.left = visPos[0] + "px";
+    posVisualizer.style.top = visPos[1] + "px";
 }
 
 function main() {
@@ -30,9 +58,12 @@ function main() {
 
     const mouse = [0, 0];
     document.addEventListener('mousemove', (event) => {
+        testMouse = [event.clientX, event.clientY];
         mouse[0] = (event.clientX / resolution[0]  * 2 - 1) * -0.02;
         mouse[1] = (event.clientY / resolution[1] * 2 - 1) * -0.02;
     });
+
+    setInterval (mouseRender, 1000/60)
         
     let nMouse = [0, 0];
     let oMouse = [0, 0];
@@ -56,9 +87,7 @@ function main() {
         twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
             nMouse[0] += (mouse[0] - nMouse[0]) * 0.02;
             nMouse[1] += (mouse[1] - nMouse[1]) * 0.02;
-                
-        posVisualizer.style.left = nMouse[0] / 0.02 * (window.innerWidth / 2) + (window.innerWidth / 2) + "px";
-        posVisualizer.style.top = nMouse[1] / 0.02 * (window.innerHeight / 2) + (window.innerHeight / 2) + "px";
+
         twgl.setUniforms(programInfo, { u_mouse: nMouse, });
         twgl.drawBufferInfo(gl, bufferInfo);
 
